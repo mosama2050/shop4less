826a31dd80183174d657ff55efea419c2c848849
mosama2050 <37184677+mosama2050@users.noreply.github.com>
2021-06-18 00:42:30 +0200

    Creating product Interface

---
+++ src/app/products/product.ts
@@ -0,0 +1,13 @@
+export interface Product {
+  id: number;
+  name:  string;
+  image: string;
+  price:  number;
+  description: string;
+  is_available:boolean,
+  quantity:number
+  brand:string;
+  color: string;
+  category_id: number;
+  rating: number;
+}
--- src/app/products/products.service.ts
+++ src/app/products/products.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
+import {Product} from "./product";
 
 @Injectable({
   providedIn: 'root'
@@ -14,25 +15,25 @@ export class ProductsService {
   }
 
 
-  public  createproduct(product: any){
-   return  this.httpClient.post(this.productUrl,product);
+  public  createproduct(product: any) :Observable<Product>{
+   return  this.httpClient.post<Product>(this.productUrl,product);
   }
 
-  public  viewproduct(productID: any){
-   return  this.httpClient.get(this.productUrl+'/'+productID);
+  public  viewproduct(productID: any):Observable<Product>{
+   return  this.httpClient.get <Product>(this.productUrl+'/'+productID);
   }
 
 
-  public updateproduct(productID: any,product: any){
-    return  this.httpClient.put(this.productUrl+'/'+productID,product);
+  public updateproduct(productID: any,product: any):Observable<Product>{
+    return  this.httpClient.put<Product>(this.productUrl+'/'+productID,product);
   }
 
-  public deleteproduct(productID: any){
-    return  this.httpClient.delete(this.productUrl+'/'+productID);
+  public deleteproduct(productID: any):Observable<Product>{
+    return  this.httpClient.delete<Product>(this.productUrl+'/'+productID);
   }
 
-  public  searchCategoryProducts(categoryId: any){
-    return  this.httpClient.get(this.productUrl+'/category/'+categoryId);
+  public  searchCategoryProducts(categoryId: any):Observable<Product>{
+    return  this.httpClient.get<Product>(this.productUrl+'/category/'+categoryId);
   }
 
   public  searchDateProducts(dateprams: any){
