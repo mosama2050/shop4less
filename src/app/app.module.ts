0872f06a705f8f9db211dfe45085de2582852313
mosama2050 <37184677+mosama2050@users.noreply.github.com>
2021-06-17 17:16:49 +0200

    create module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import {OrdersModule} from "./orders/orders.module";
+import {AppRoutingModule} from "./app-routing.module";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    OrdersModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
